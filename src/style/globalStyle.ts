78685eb2b41dd54deaab2b3e7f85c13d93b73bcc
BK <3031011@naver.com>
2023-12-15 00:53:40 +0900

    design: 공통 background style 적용

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -1,5 +1,7 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import mainBackground from '../assets/background.png';
+import textBackground from '../assets/background-title.png';
 
 export default createGlobalStyle`
 ${reset}
@@ -8,31 +10,69 @@ ${reset}
   box-sizing: border-box;
   font-family: 'Pretendard Variable' !important;
 }
+
 html {
   font-size: 62.5%;
   /* rem 값의 기준을 잡기 위함 16px -> 10px */
   background-color: #F4F6F9;
 }
+
 body {
   margin: 0;
+  background: url(${mainBackground}) no-repeat center / cover;
 }
+
 input, textarea { 
   -moz-user-select: auto;
   -webkit-user-select: auto;
   -ms-user-select: auto;
   user-select: auto;
 }
+
 input:focus {
   outline: none;
 }
+
 button {
   border: none;
   background: none;
   padding: 0;
   cursor: pointer;
 }
-a{
+
+a {
   text-decoration: none;
   color: inherit;
+}
+
+#root {
+  max-width: 420px;
+  min-height: 100vh;
+  background: #fff;
+  margin-left: 50%;
+}
+
+img {
+  max-width: 100%;
+  vertical-align: top;
+}
+
+@media screen and (max-width: 868px) {
+  #root {
+    margin: 0 auto;
+  };
+}
+
+@media screen and (min-width: 868px) {
+  body::before{
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    transform: translateY(-50%);
+    background: url(${textBackground}) no-repeat center/contain;
+    width: 385px;
+    height: 238px;
+  } 
 }
 `;
