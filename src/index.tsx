717ddf4b67a1cde11d4be4d96026f540bb4fcfca
BK <3031011@naver.com>
2023-12-15 13:30:52 +0900

    refactor: provider, router index -> app으로 이동

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import store from 'store/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const queryClient = new QueryClient();
 
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </QueryClientProvider>
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
