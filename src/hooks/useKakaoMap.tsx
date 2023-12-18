b54704ed69eee362b44438b348fc6eefd0010142
zxxng <jeayeong123@naver.com>
2023-12-18 12:46:21 +0900

    type: 카카오지도 훅 타입 수정 #4

--- src/hooks/useKakaoMap.tsx
+++ src/hooks/useKakaoMap.tsx
@@ -1,5 +1,5 @@
 import { RefObject, useEffect, useState } from 'react';
-import { TLocation, getMyLocation } from 'utils/getMyLocation';
+import { ILocation, getMyLocation } from 'utils/getMyLocation';
 
 const useKakaoMap = (mapContainer: RefObject<HTMLDivElement>) => {
   const [map, setMap] = useState<any>(null);
@@ -10,7 +10,7 @@ const useKakaoMap = (mapContainer: RefObject<HTMLDivElement>) => {
         let center;
         if (navigator.geolocation) {
           try {
-            const { latitude, longitude }: TLocation = await getMyLocation();
+            const { latitude, longitude }: ILocation = await getMyLocation();
             center = new window.kakao.maps.LatLng(latitude, longitude);
           } catch (error) {
             console.error(error);
