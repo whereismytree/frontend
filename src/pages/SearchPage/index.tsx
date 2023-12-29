dd214314e5974126c437d53e3cf02e32e8e2afc2
BK <3031011@naver.com>
2023-12-30 00:22:04 +0900

    feat: 검색 페이지 구현 완료

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import Search from 'components/Search';
+import React from 'react';
 
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <Search>
+      <Search.Header>
+        <Search.Input />
+        <Search.CurrentLocationButton />
+      </Search.Header>
+      <Search.Result />
+    </Search>
+  );
 };
