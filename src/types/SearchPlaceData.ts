f48bb353980295331f1f69b281b29f3aec1dd25f
BK <3031011@naver.com>
2023-12-30 00:11:20 +0900

    type: 주소 검색 결과 데이터 타입 수정
    주소 나타내는 x, y좌표 추가

--- src/types/SearchPlaceData.ts
+++ src/types/SearchPlaceData.ts
@@ -1,7 +1,10 @@
 interface ISearchPlaceData {
   id: string;
   place_name: string;
+  address_name: string;
   road_address_name: string;
+  x: number;
+  y: number;
 }
 
 export default ISearchPlaceData;
