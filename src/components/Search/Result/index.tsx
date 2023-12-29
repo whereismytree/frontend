da3015e72215aa52b3622cf7a0d156e424406b0e
BK <3031011@naver.com>
2023-12-30 00:54:26 +0900

    feat: 검색어 없을 시 검색 팁 보여주는 기능 추가 #17

--- src/components/Search/Result/index.tsx
+++ src/components/Search/Result/index.tsx
@@ -1,8 +1,22 @@
+import React from 'react';
 import Item from 'components/common/Item';
 import { useSelector } from 'react-redux';
 import ISearchPlaceData from 'types/SearchPlaceData';
 import usePlaceSearch from 'hooks/usePlaceSearch';
 import * as S from './style';
+
+function SearchTip() {
+  return (
+    <S.SearchTip>
+      <strong>검색 Tip!</strong>
+      <span>
+        트리가 실내에 있는 경우,
+        <br /> 건물이나 상호명으로 검색해 보세요!
+      </span>
+      <span>예) OO백화점, OO카페</span>
+    </S.SearchTip>
+  );
+}
 
 function SearchListItem({ data }: { data: ISearchPlaceData }) {
   return (
@@ -18,21 +32,22 @@ function SearchListItem({ data }: { data: ISearchPlaceData }) {
   );
 }
 
-function SearchResultList() {
-  const { keyword } = useSelector(({ search }) => search);
-  const result = usePlaceSearch(keyword);
-
+function SearchResultList({ result }: { result: ISearchPlaceData[] }) {
   return (
-    keyword &&
-    result &&
-    result.length > 0 && (
-      <S.SearchResult>
-        {result.map((data: ISearchPlaceData) => {
-          return <SearchListItem key={data.id} data={data} />;
-        })}
-      </S.SearchResult>
-    )
+    <S.SearchResult>
+      {result.map((data: ISearchPlaceData) => {
+        return <SearchListItem key={data.id} data={data} />;
+      })}
+    </S.SearchResult>
   );
 }
 
-export default SearchResultList;
+function Search() {
+  const { keyword } = useSelector(({ search }) => search);
+  const result = usePlaceSearch(keyword);
+  const isHaveResult = keyword && result && result.length > 0;
+
+  return isHaveResult ? <SearchResultList result={result} /> : <SearchTip />;
+}
+
+export default Search;
