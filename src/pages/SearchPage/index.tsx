abef2b70925c76df571ab5b546e2c3b41ce48a6e
zxxng <jeayeong123@naver.com>
2024-01-08 17:51:08 +0900

    feat/키워드 입력 후 엔터 입력시 트리 목록 조회되는 기능 추가 #24

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
+import SearchInput from 'components/main/SearchInput';
+import { ITreeItem } from 'types/apiResponse';
+// import useApiQuery from 'hooks/useApiQuery';
+import TreeImageItem from 'components/TreeImageItem';
+import treeJSON from 'assets/treedata.json';
 
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [keyword, setKeyword] = useState<string>('');
+  // TODO: 추후 서버 통신으로 변경 필요, 현재는 서버에 데이터가 없어 JSON 활용했습니다
+  const data: ITreeItem[] = treeJSON.trees;
+  // const { data } = useApiQuery<ITreeItem[]>(`v1/trees/list?name=${keyword}&address=${keyword}`);
+
+  return (
+    <>
+      <SearchInput setKeyword={setKeyword} />
+      {data && keyword ? (
+        <section>
+          {data.map((tree) => {
+            return (
+              <TreeImageItem key={tree.name} location={tree.roadAddress}>
+                {tree.name}
+              </TreeImageItem>
+            );
+          })}
+        </section>
+      ) : null}
+    </>
+  );
 };
