f7d1196ebe7fbd60b57dd18966768115d3c5cf3e
BK <3031011@naver.com>
2024-01-15 15:57:44 +0900

    fix: 삭제된 코드 복구

--- src/pages/ErrorPage/style.ts
+++ src/pages/ErrorPage/style.ts
@@ -1,1 +1,9 @@
-export {};
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  background-color: var(--grey-light);
+`;
