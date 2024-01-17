bdf864719b6df9e4f92b73ed4ed3d39736fb77b8
BK <3031011@naver.com>
2024-01-18 00:10:05 +0900

    feat: 등록한 트리가 없을 시 가이드 컴포넌트 렌더링

--- src/components/TreeList/index.tsx
+++ src/components/TreeList/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import TreeReviewItem from 'components/TreeReviewItem';
-import ListTitle from 'components/common/ListTitle';
 import * as S from './style';
 
 type ITreeList = {
@@ -16,13 +15,13 @@ function TreeList({ list, type }: { list: ITreeList; type: 'registed' | 'saved' 
   const [viewList, setViewList] = useState<boolean>(true);
 
   return (
-    <S.RegistedTree $viewList={viewList}>
+    <S.RegistedTree viewList={viewList}>
       <S.ViewButton type="button" onClick={() => setViewList((prev) => !prev)}>
         {viewList ? '지도' : '목록'}보기
       </S.ViewButton>
-      <S.TitleSection>
-        <ListTitle count={list.length}>{type === 'registed' ? '등록' : '저장'}한 트리</ListTitle>
-      </S.TitleSection>
+      <S.Title>
+        {type === 'registed' ? '등록' : '저장'}한 트리<S.GreenText>{list.length}</S.GreenText>
+      </S.Title>
       <S.RegistTreeList>
         {list.map(({ treeId, name, address, reviewsCount }) => (
           <S.RegistTreeItem key={treeId}>
