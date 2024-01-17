416a3853967704f8ddbe251cecf1e4b11608f655
BK <3031011@naver.com>
2024-01-17 13:17:06 +0900

    design: topbar 추가

--- src/pages/SignIn/ProfileSetting/style.ts
+++ src/pages/SignIn/ProfileSetting/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.form`
   position: relative;
   width: calc(100% - 120px);
-  margin-top: 50%;
+  margin-top: 30%;
   left: 50%;
   transform: translateX(-50%);
   display: flex;
