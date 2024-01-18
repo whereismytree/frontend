1d4f5eb3af04e836ddb9b51802301b0c2025dcec
BK <3031011@naver.com>
2024-01-18 21:23:08 +0900

    refactor: 반복되어 사용되는 리스트 제목 UI ListTitle 컴포넌트로 분리

--- src/components/common/ListTitle/style.ts
+++ src/components/common/ListTitle/style.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const Title = styled.h2`
+  padding: 14px 25px;
   font-size: 1.4rem;
   font-weight: 800;
+  border-bottom: 1px solid var(--grey-light);
 `;
 
 export const GreenText = styled.span`
