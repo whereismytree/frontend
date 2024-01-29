e27854661664107d4373114dd56c2721d223cf63
BK <3031011@naver.com>
2024-01-29 13:20:32 +0900

    fix: 스타일 컴포넌트에서 발생하는 에러 해결 #30
    '$' prefix add

--- src/components/common/KebabButton/index.tsx
+++ src/components/common/KebabButton/index.tsx
@@ -9,7 +9,7 @@ function KebabButton({
   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }) {
   return (
-    <S.VerticalButton isOpen={isOpen} onClick={(e) => onClick(e)}>
+    <S.VerticalButton $isOpen={isOpen} onClick={(e) => onClick(e)}>
       <img src={vertical} alt="더보기" />
     </S.VerticalButton>
   );
