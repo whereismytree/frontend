6e6d2e6e6bcd1ca0aa6f41027158297e75e2c269
BK <3031011@naver.com>
2024-01-31 19:43:51 +0900

    format: 커스텀 훅 구조 개선
    에러 처리 로직 커스텀 에러로 세분화 하여 개선
    useDeleteReview 커스텀 훅은 최종 사용자가 전달하는 옵션을 조금만 알 수 있도록 구조 개선

--- src/hooks/reviewHooks.ts
+++ src/hooks/reviewHooks.ts
@@ -1,4 +1,5 @@
-import { HttpError } from 'types/ErrorTypes';
+import { HTTPError } from 'error/HTTPError';
+import { MutateOptions } from '@tanstack/react-query';
 import useApiMutation from './useApiMutation';
 import useApiQuery from './useApiQuery';
 
@@ -14,21 +15,29 @@ interface ReviewResponse {
 }
 
 export const useReviewData = (reviewId: string) => {
-  const { data, isError } = useApiQuery<ReviewResponse>(`v1/reviews/${reviewId}`);
+  const { data, isError, isLoading } = useApiQuery<ReviewResponse>(`v1/reviews/${reviewId}`);
 
-  if (isError) throw new HttpError('리뷰를 불러오는 데 오류가 발생했습니다.');
-  if (!data) throw new Error(`리뷰 데이터를 받아오는 API 응답값이 올바르지 않습니다. ${data}`);
+  if (isError) {
+    throw new HTTPError('리뷰를 불러오는 데 오류가 발생했습니다.');
+  }
+
+  if (!isLoading && !data) {
+    throw new HTTPError('리뷰 데이터를 받아오는 API 응답 값이 올바르지 않습니다.');
+  }
 
   return data;
 };
 
-export const useReviewDelete = (reviewId: string) => {
-  return useApiMutation(`v1/reviews/${reviewId}`, 'DELETE', {
-    onSuccess: () => {
-      // TODO: 리뷰를 삭제하는 요청이 성공적으로 완료 되었을 시 어떠한 경로로 라우팅 해주면 될 것 같아요.
-    },
-    onError: () => {
-      throw new Error('리뷰를 삭제하던 중 오류가 발생했습니다.');
-    },
-  });
+export const useDeleteReview = (reviewId: string) => {
+  const { mutate, isError } = useApiMutation(`v1/reviews/${reviewId}`, 'DELETE');
+
+  if (isError) {
+    throw new HTTPError('리뷰를 삭제하던 중 오류가 발생했습니다.');
+  }
+
+  const deleteReview = (options: MutateOptions<unknown, unknown, unknown, unknown>) => {
+    return mutate(null, options);
+  };
+
+  return deleteReview;
 };
