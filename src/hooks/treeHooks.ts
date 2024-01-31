22f011d37ddd7c61351c9f40305b01658936fe74
BK <3031011@naver.com>
2024-01-31 19:43:51 +0900

    format: 커스텀 훅 구조 개선
    에러 처리 로직 커스텀 에러로 세분화 하여 개선
    useDeleteReview 커스텀 훅은 최종 사용자가 전달하는 옵션을 조금만 알 수 있도록 구조 개선

--- src/hooks/treeHooks.ts
+++ src/hooks/treeHooks.ts
@@ -1,12 +1,17 @@
+import { HTTPError } from 'error/HTTPError';
 import { ITreeItem } from 'types/apiResponse';
-import { HttpError } from 'types/ErrorTypes';
 import useApiQuery from './useApiQuery';
 
 export const useTreeData = (treeId: string) => {
-  const { data, isError } = useApiQuery<ITreeItem>(`v1/trees/${treeId}`);
+  const { data, isError, isLoading, error } = useApiQuery<ITreeItem>(`v1/trees/${treeId}`);
 
-  if (isError) throw new HttpError('트리를 불러오는 데 오류가 발생했습니다.');
-  if (!data) throw new Error(`트리 데이터를 받아오는 API 응답값이 올바르지 않습니다. ${data}`);
+  if (isError) {
+    throw new HTTPError(`트리를 불러오는 데 오류가 발생했습니다. ${error}`);
+  }
+
+  if (!isLoading && !data) {
+    throw new Error(`트리 데이터를 받아오는 API 응답값이 올바르지 않습니다. ${data}`);
+  }
 
   return data;
 };
