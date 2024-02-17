e005d14bbf853c50deecec14805dd53dbbdf2917
BK <3031011@naver.com>
2024-02-17 19:53:56 +0900

    chore: 주석 제거

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -44,10 +44,6 @@ export const Router = () => {
   const treeRoute: RouteObject = {
     path: `${PATH.treePage.root}/*`,
     element: <Outlet />,
-    // TreeInfo 컴포넌트 내부에서, state 데이터를 받아서 처리
-    // 언제 동적 파라미터를 사용하고, 언제 useLocation의 state를 사용하면 좋은지?
-    // useLocation의 state를 사용하면 복잡한 데이터를 다시 로드하지 않아도 됨.
-    // 그런데 이런 경우에 공유하기 기능을 사용할 수 없을 것 같아요. 외부에서 진입하면 데이터가 없음
     children: [
       { path: PATH.treePage.children.dynamicParam, element: <TreeInfo /> },
       {
@@ -67,7 +63,6 @@ export const Router = () => {
     element: <Outlet />,
     children: [
       { path: PATH.reviewPage.dynamicParam, element: <ReviewDetailPage /> },
-      // 리뷰 등록/수정에 트리 아이디 혹은 리뷰 아이디가 필요한데 동적으로 매칭할지, state에 담아서 보낼지?
       // 리뷰 등록 페이지
       { path: `${PATH.reviewPage.children.regist}/:treeId`, element: null },
       // 리뷰 수정 페이지
