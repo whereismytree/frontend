e6077903414a4d8c4d17c418c427ce22db340d56
BK <3031011@naver.com>
2024-02-20 22:41:11 +0900

    refactor: SnackBar 컴포넌트 커스텀 훅으로 분리해 사용할 수 있도록 코드 리팩토링

---
+++ src/hooks/useSnackBar.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from 'react';
+import styled, { css, keyframes } from 'styled-components';
+
+const showAndSlideUp = keyframes`
+  0% {
+    top: calc(var(--header-height) - 20px);
+    opacity: 0.4;
+  }
+
+  5% {
+    opacity: 1;
+    top: calc(var(--header-height) + 20px);
+  }
+
+  90% {
+    opacity: 1;
+    top: calc(var(--header-height) + 20px);
+  }
+
+  95% {
+    opacity: 0;
+  }
+
+  100% {
+    top: 0;
+  }
+  `;
+
+const SnackBarUI = styled.span<{ $view: boolean; $during: number }>`
+  position: fixed;
+  top: var(--header-height);
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 100;
+  font-size: 1.2rem;
+  color: var(--main-white);
+  background-color: var(--main-green);
+  border: 1px solid var(--grey-dark);
+  border-radius: 20px;
+  padding: 12px 20px;
+  opacity: 0;
+
+  ${({ $view, $during }) =>
+    $view &&
+    css`
+      animation: ${showAndSlideUp} ${$during}ms linear;
+    `}
+`;
+
+const useSnackBar = () => {
+  const [view, setView] = useState(false);
+  const [during, setDuring] = useState(0);
+
+  const render = (sec: number) => {
+    setView(true);
+    const ms = sec * 1000;
+    setDuring(ms);
+    setTimeout(() => setView(false), ms);
+  };
+
+  const SnackBar = ({ children }: { children: string }) => {
+    return (
+      <SnackBarUI $view={view} $during={during}>
+        {children}
+      </SnackBarUI>
+    );
+  };
+
+  return { SnackBar, render };
+};
+
+export default useSnackBar;
