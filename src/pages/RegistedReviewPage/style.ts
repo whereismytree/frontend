f55fc0e0d230902493f8f69ec04ccc9e137227d7
BK <3031011@naver.com>
2024-02-20 14:45:04 +0900

    rename: 유저의 활동과 관련된 페이지들 디렉토리명 수정
    RegistReviewPage -> MyReviewPage
    RegistedPage -> MyTreePage
    SavePage -> SvaedTreePage

--- src/pages/RegistedReviewPage/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.main`
-  display: flex;
-  align-items: center;
-  height: calc(100vh - var(--navbar-height) - var(--header-height));
-  background-color: var(--grey-light);
-`;
