b478f0ce1ae7a0a9c0bc71eb58bcdb964e542d64
BK <3031011@naver.com>
2024-02-21 23:00:49 +0900

    refactor: 닉네임 설정 페이지 코드 리팩토링

--- src/pages/LoginPage/ProfileSetting/Nickname/style.ts
+++ src/pages/LoginPage/ProfileSetting/Nickname/style.ts
@@ -1,46 +1,9 @@
 import styled from 'styled-components';
-import check from 'assets/check.svg';
 
 export const GuideText = styled.p`
   font-size: 1.6rem;
   font-weight: 800;
   margin: 25px 0 16px;
-`;
-
-const validTextStyle = styled.p`
-  position: absolute;
-  bottom: -20px;
-
-  &::before {
-    content: '';
-    width: 13px;
-    height: 13px;
-    border-radius: 50%;
-    display: inline-block;
-    vertical-align: middle;
-    margin-right: 6px;
-  }
-`;
-
-export const InvalidText = styled(validTextStyle)`
-  color: #b5002d;
-
-  &::before {
-    content: 'X';
-    text-align: center;
-    line-height: 14px;
-    color: #fff;
-    background-color: #b5002d;
-  }
-`;
-
-export const ValidText = styled(validTextStyle)`
-  color: var(--main-green);
-
-  &::before {
-    background: url(${check}) no-repeat center;
-    background-color: var(--main-green);
-  }
 `;
 
 export const InputWrapper = styled.div`
@@ -51,25 +14,18 @@ export const InputWrapper = styled.div`
   margin-bottom: 55px;
 `;
 
-const borderColor = (state: boolean | null) => {
-  switch (state) {
-    case true:
-      return '#B5002D';
-
-    case false:
-      return 'var(--main-green)';
-
-    default:
-      return 'var(--grey-light)';
-  }
-};
-
-export const Input = styled.input<{ $isError: null | boolean }>`
+export const Input = styled.input<{ $isValid: boolean; $isEmpty: boolean }>`
   font-size: 1.2rem;
   border-radius: 5px;
   padding: 8px 14px;
-  border: 1px solid ${({ $isError }) => borderColor($isError)};
   width: 100%;
+
+  border: 1px solid
+    ${({ $isValid, $isEmpty }) => {
+      if ($isEmpty) return 'var(--grey-light)';
+      if ($isValid) return 'var(--main-green)';
+      return '#B5002D';
+    }};
 `;
 
 export const DupButton = styled.button`
