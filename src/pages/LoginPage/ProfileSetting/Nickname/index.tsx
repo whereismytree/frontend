da435c5d04e06f23307ed98689cb98b390541fe2
BK <3031011@naver.com>
2024-02-21 23:00:49 +0900

    refactor: 닉네임 설정 페이지 코드 리팩토링

--- src/pages/LoginPage/ProfileSetting/Nickname/index.tsx
+++ src/pages/LoginPage/ProfileSetting/Nickname/index.tsx
@@ -1,64 +1,69 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import axios from 'axios';
 import Button from 'components/common/button';
+import useDebouncedCallback from 'hooks/useDebouncedCallback';
+import ValidateResult from './ValidateResult';
 import * as S from './style';
 
+const NICKNAME_LENGTH = { min: 2, max: 8 };
+const NICKNAME_FORM_NAME = 'nickname';
+// 한글과 숫자로만 이루어져 있는지 확인하는 정규식
+const KorAndNumRegex = /^[가-힣\d]+$/i;
+
+const validateOptions = {
+  minLength: {
+    value: NICKNAME_LENGTH.min,
+    message: `닉네임은 최소 ${NICKNAME_LENGTH.min}글자, 최대 ${NICKNAME_LENGTH.max}글자여야 합니다.`,
+  },
+  maxLength: {
+    value: NICKNAME_LENGTH.max,
+    message: `닉네임은 최소 ${NICKNAME_LENGTH.min}글자, 최대 ${NICKNAME_LENGTH.max}글자여야 합니다.`,
+  },
+  pattern: {
+    value: KorAndNumRegex,
+    message: '닉네임은 한글과 숫자로만 이루어져야 합니다.',
+  },
+};
+
+const checkNicknameDuplicate = async (nickname: string) => {
+  const {
+    data: { available },
+  } = await axios.get(
+    `${process.env.REACT_APP_TREE_API_URL}v1/my/check/nickname?nickname=${nickname}`,
+  );
+
+  return !available;
+};
+
 function NicknameSetting() {
-  const { register, getValues } = useFormContext();
-  const [nicknameError, setNicknameError] = useState<{ error: null | boolean; message: string }>({
-    error: null,
-    message: '',
+  const {
+    register,
+    getValues,
+    watch,
+    setError,
+    formState: { errors },
+  } = useFormContext();
+  const isHaveNickname = !!watch(NICKNAME_FORM_NAME);
+  const isValidNickname = !Object.prototype.hasOwnProperty.call(errors, NICKNAME_FORM_NAME);
+  const [isAvailable, setAvailable] = useState(false);
+  const lastCheckedNickname = useRef('');
+  // 디바운싱 활용, 마지막으로 중복확인한 닉네임과 현재 인풋창에 입력되어 있는 값이 동일한지 확인 후 유효성 상태에 반영
+  const checkIfSameAsLastCheckedNickname = useDebouncedCallback((currentNickname: string) => {
+    setAvailable(currentNickname === lastCheckedNickname.current);
   });
 
-  const handleDupButtonClick = async (nickname: string) => {
-    if (!validateNickname(nickname)) return;
-    const isDupNickname = await checkDuplicateNickname(nickname);
+  const handleDupBtnClick = async (nickname: string) => {
+    const isDuplicate = await checkNicknameDuplicate(nickname);
 
-    if (isDupNickname) {
-      setNicknameError({
-        error: true,
+    if (isDuplicate) {
+      setError(NICKNAME_FORM_NAME, {
         message: '다른 사람이 쓰고 있어요! 다른 닉네임을 정해주세요',
       });
     } else {
-      setNicknameError({ error: false, message: '좋아요!' });
+      setAvailable(true);
+      lastCheckedNickname.current = nickname;
     }
-  };
-
-  const validateNickname = (nickname: string) => {
-    const MIN_NICKNAME_LENGTH = 2;
-    const MAX_NICKNAME_LENGTH = 8;
-
-    if (nickname.length < 2 || nickname.length > 8) {
-      setNicknameError({
-        error: true,
-        message: `닉네임은 최소 ${MIN_NICKNAME_LENGTH}글자, 최대 ${MAX_NICKNAME_LENGTH}글자여야 합니다.`,
-      });
-
-      return false;
-    }
-
-    if (!/^[가-힣\d]+$/i.test(nickname)) {
-      setNicknameError({
-        error: true,
-        message: '닉네임은 한글과 숫자로만 이루어져야 합니다.',
-      });
-
-      return false;
-    }
-
-    setNicknameError({ error: false, message: '' });
-    return true;
-  };
-
-  const checkDuplicateNickname = async (nickname: string) => {
-    const {
-      data: { available },
-    } = await axios.get(
-      `${process.env.REACT_APP_TREE_API_URL}v1/my/check/nickname?nickname=${nickname}`,
-    );
-
-    return !available;
   };
 
   return (
@@ -66,31 +71,33 @@ function NicknameSetting() {
       <S.GuideText>닉네임을 설정해주세요.</S.GuideText>
       <S.InputWrapper>
         <S.Input
-          placeholder="닉네임을 입력해주세요"
           type="text"
-          $isError={nicknameError.error}
-          {...register('nickname', {
+          placeholder="닉네임을 입력해주세요"
+          $isEmpty={!isHaveNickname}
+          $isValid={isValidNickname}
+          {...register(NICKNAME_FORM_NAME, {
             required: true,
+            onChange: (e) => checkIfSameAsLastCheckedNickname(e.target.value),
+            ...validateOptions,
           })}
         />
+
         <S.DupButton
           type="button"
-          onClick={() => {
-            const nickname = getValues('nickname');
-            handleDupButtonClick(nickname);
-          }}
+          onClick={() => handleDupBtnClick(getValues(NICKNAME_FORM_NAME))}
+          disabled={!!true}
         >
           중복확인
         </S.DupButton>
 
-        {nicknameError.error !== null && nicknameError.error && (
-          <S.InvalidText>{nicknameError.message}</S.InvalidText>
+        {isHaveNickname && (
+          <ValidateResult isValid={isValidNickname}>
+            {errors[NICKNAME_FORM_NAME]?.message as string}
+          </ValidateResult>
         )}
-        {nicknameError.error !== null && !nicknameError.error && <S.ValidText>좋아요!</S.ValidText>}
       </S.InputWrapper>
-      {nicknameError.error !== null && !nicknameError.error && (
-        <Button.Small type="submit">다음으로</Button.Small>
-      )}
+
+      {isAvailable && <Button.Small type="submit">다음으로</Button.Small>}
     </>
   );
 }
