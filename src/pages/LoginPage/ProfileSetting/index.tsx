7c1c326173e316736d1de0a15155f1f9adb05121
BK <3031011@naver.com>
2024-02-22 16:55:10 +0900

    refactor: 프로필 등록 페이지 구조 개선

--- src/pages/LoginPage/ProfileSetting/index.tsx
+++ src/pages/LoginPage/ProfileSetting/index.tsx
@@ -1,51 +1,44 @@
 import { FormProvider, useForm } from 'react-hook-form';
-import ProfileImageSetting from 'pages/LoginPage/ProfileSetting/ProfileImage';
-import NicknameSetting from 'pages/LoginPage/ProfileSetting/Nickname';
-import useApiMutation from 'hooks/useApiMutation';
+import ProfileImageSetting from 'pages/LoginPage/ProfileSetting/components/ImageSetting';
 import { useNavigate } from 'react-router-dom';
 import getPath from 'utils/getPath';
 import Topbar from 'components/Topbar';
+import { useProfile } from './hooks';
+import ProfileSettingProvider from './provider';
+import { ICreateProfileAPIBody } from './types';
 import * as S from './style';
+import SubmitButton from './components/SubmitButton';
+import NicknameSetting from './components/NicknameSetting';
 
-interface IProfile {
-  nickname: string;
-  profileImageUrl: string;
-}
-
-function Nickname() {
-  const methods = useForm<IProfile>({ mode: 'onChange' });
-  const { handleSubmit } = methods;
+function ProfileSetting() {
   const navigate = useNavigate();
+  const { create } = useProfile();
+  const methods = useForm<ICreateProfileAPIBody>({ reValidateMode: 'onChange' });
+  const { handleSubmit } = methods;
 
-  const { mutate } = useApiMutation<{ nickname: string; profileImageUrl: string }>(
-    'v1/my/profile',
-    'POST',
-    {
-      onSuccess: () => {
-        navigate(getPath('mainPage', 'root'));
+  const createProfile = (data: Omit<ICreateProfileAPIBody, 'profileImageUrl'>) => {
+    create(
+      { ...data, profileImageUrl: 'http://s3.example.com/image1' },
+      {
+        onSuccess: () => navigate(getPath('mainPage', 'root')),
       },
-
-      onError: (e) => {
-        throw new Error(`'닉네임을 설정하던 도중 오류가 발생했습니다.' ${e.message}`);
-      },
-    },
-  );
-
-  const createProfile = (data: Omit<IProfile, 'profileImageUrl'>) => {
-    mutate({ ...data, profileImageUrl: 'http://s3.example.com/image1' });
+    );
   };
 
   return (
     <>
       <Topbar.Icon type="cookie" />
       <FormProvider {...methods}>
-        <S.Wrapper onSubmit={handleSubmit(createProfile)}>
-          <ProfileImageSetting />
-          <NicknameSetting />
-        </S.Wrapper>
+        <ProfileSettingProvider>
+          <S.Wrapper onSubmit={handleSubmit(createProfile)}>
+            <ProfileImageSetting />
+            <NicknameSetting />
+            <SubmitButton />
+          </S.Wrapper>
+        </ProfileSettingProvider>
       </FormProvider>
     </>
   );
 }
 
-export default Nickname;
+export default ProfileSetting;
