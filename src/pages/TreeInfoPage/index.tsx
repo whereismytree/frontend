d0874aad4218cd03f712691e509db3816ff3a0cb
zxxng <jeayeong123@naver.com>
2024-02-24 01:35:42 +0900

    feat: 상세페이지 지도 마커 표시 기능 추가

--- src/pages/TreeInfoPage/index.tsx
+++ src/pages/TreeInfoPage/index.tsx
@@ -1,5 +1,4 @@
-import React, { useRef } from 'react';
-import useKakaoMap from 'hooks/useKakaoMap';
+import React, { useState, useEffect, useRef } from 'react';
 import Topbar from 'components/Topbar';
 import TreeTitle from 'components/treeinfo/TreeTitle';
 import TreeDetails from 'components/treeinfo/TreeDetails';
@@ -9,16 +8,38 @@ import Button from 'components/common/button';
 import { useLocation } from 'react-router-dom';
 import { ITreeItem } from 'types/apiResponse';
 import useApiQuery from 'hooks/useApiQuery';
+import treeMarker from 'assets/tree_marker.svg';
 import * as S from './style';
 
 export const TreeInfo = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useKakaoMap(mapContainer);
   const location = useLocation();
   const treeId = location.pathname.split('/')[2];
   const { data } = useApiQuery<ITreeItem>(`v1/trees/${treeId}`);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [map, setMap] = useState<any>(null);
 
-  console.log(map);
+  useEffect(() => {
+    if (data) {
+      window.kakao.maps.load(async () => {
+        const center = new window.kakao.maps.LatLng(data.lat, data.lng);
+        const options = { center, level: 3 };
+        if (mapContainer.current) {
+          const map = new window.kakao.maps.Map(mapContainer.current, options);
+          const imgSize = new window.kakao.maps.Size([24, 24]);
+          const markerImage = new window.kakao.maps.MarkerImage(treeMarker, imgSize);
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const marker = new window.kakao.maps.Marker({
+            position: new window.kakao.maps.LatLng(data.lat, data.lng),
+            map,
+            title: data.name,
+            image: markerImage,
+          });
+          setMap(map);
+        }
+      });
+    }
+  }, [data]);
 
   return data ? (
     <>
