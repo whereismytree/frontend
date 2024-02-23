c52c59a238d285bf68f0a3467fbf43445465dff3
zxxng <jeayeong123@naver.com>
2024-02-24 04:35:07 +0900

    feat: 트리 리뷰 등록 api 연동

--- src/pages/ReviewRegistAndEditPage/components/Tag/style.ts
+++ src/pages/ReviewRegistAndEditPage/components/Tag/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 interface StyledProps {
-  isSelected: boolean;
+  isselected: boolean;
 }
 
 export const Wrapper = styled.button<StyledProps>`
@@ -16,14 +16,14 @@ export const Wrapper = styled.button<StyledProps>`
   border-radius: 1rem;
   padding: 0.85rem 1.2rem;
   box-sizing: border-box;
-  background-color: ${({ isSelected }) => (isSelected ? 'var(--main-green)' : 'white')};
+  background-color: ${({ isselected }) => (isselected ? 'var(--main-green)' : 'white')};
   transition: all 0.2s;
 `;
 
 export const Text = styled.p<StyledProps>`
   font-size: 1.4rem;
   font-weight: 300;
-  color: ${({ isSelected }) => (isSelected ? 'white' : 'var(--main-black)')};
+  color: ${({ isselected }) => (isselected ? 'white' : 'var(--main-black)')};
 `;
 
 export const TagImg = styled.img`
