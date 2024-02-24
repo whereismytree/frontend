379137edd3868209e9e07c5dba7e1be79c06df88
zxxng <jeayeong123@naver.com>
2024-02-25 02:03:36 +0900

    feat: 이미지 추가 기능 구현

--- src/pages/ReviewRegistAndEditPage/components/ReviewForm/index.tsx
+++ src/pages/ReviewRegistAndEditPage/components/ReviewForm/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import photoIcon from 'assets/photo-icon.svg';
+import test from 'assets/treeinfo-default.svg';
 import * as S from './style';
 
 interface IReviewFormProp {
@@ -9,21 +10,49 @@ const ReviewForm = ({ contentRef }: IReviewFormProp) => {
   const [textLength, setTextLength] = useState<number>(
     contentRef.current ? contentRef.current.value.length : 0,
   );
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+
   const handleOnInput = () => {
     if (contentRef.current) {
       setTextLength(contentRef.current.value.length);
     }
   };
 
+  const handleDeletePhoto = () => {
+    setSelectedFiles(null);
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    e.persist();
+    console.log(selectedFiles);
+    console.log(e.target.files);
+    setSelectedFiles(e.target.files);
+  };
+
   return (
     <S.Wrapper>
       <S.Title>리뷰를 남겨주세요</S.Title>
-      <S.ButtonContainer>
-        <S.PhotoButton>
-          <S.PhotoIcon src={photoIcon} alt="사진 추가하기" />
-          사진 추가하기
-        </S.PhotoButton>
-      </S.ButtonContainer>
+      {selectedFiles ? (
+        <S.PhotoContainer>
+          <S.Photo src={test} alt="ff" />
+          <S.PhotoDeleteButton onClick={handleDeletePhoto} />
+        </S.PhotoContainer>
+      ) : (
+        <S.ButtonContainer>
+          <S.PhotoInput
+            type="file"
+            name="images"
+            accept=".png, .jpg,image/*"
+            id="photo-upload"
+            onChange={(e) => handleFileChange(e)}
+          />
+          <S.PhotoButton htmlFor="photo-upload">
+            <S.PhotoIcon src={photoIcon} alt="사진 추가하기" />
+            사진 추가하기
+          </S.PhotoButton>
+        </S.ButtonContainer>
+      )}
       <S.TextBox
         ref={contentRef}
         onInput={handleOnInput}
