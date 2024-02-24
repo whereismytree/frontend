5ca3f8fe43769bd73244f88370675f3a0e79a5bf
zxxng <jeayeong123@naver.com>
2024-02-25 00:28:38 +0900

    feat: useKakaoMap에 지도 중심 좌표 받아오는 기능 추가

--- src/pages/TreeInfoPage/index.tsx
+++ src/pages/TreeInfoPage/index.tsx
@@ -9,37 +9,50 @@ import { useLocation } from 'react-router-dom';
 import { ITreeItem } from 'types/apiResponse';
 import useApiQuery from 'hooks/useApiQuery';
 import treeMarker from 'assets/tree_marker.svg';
+import useKakaoMap from 'hooks/useKakaoMap';
 import * as S from './style';
+
+type TPosition = number | undefined;
+interface ILatLng {
+  lat: TPosition;
+  lng: TPosition;
+}
 
 export const TreeInfo = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const treeId = location.pathname.split('/')[2];
+  const [latLng, setLatLng] = useState<ILatLng>();
   const { data } = useApiQuery<ITreeItem>(`v1/trees/${treeId}`);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [map, setMap] = useState<any>(null);
+  const map = useKakaoMap(mapContainer, latLng);
 
   useEffect(() => {
-    if (data) {
-      window.kakao.maps.load(async () => {
-        const center = new window.kakao.maps.LatLng(data.lat, data.lng);
-        const options = { center, level: 3 };
-        if (mapContainer.current) {
-          const map = new window.kakao.maps.Map(mapContainer.current, options);
-          const imgSize = new window.kakao.maps.Size([24, 24]);
-          const markerImage = new window.kakao.maps.MarkerImage(treeMarker, imgSize);
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const marker = new window.kakao.maps.Marker({
-            position: new window.kakao.maps.LatLng(data.lat, data.lng),
-            map,
-            title: data.name,
-            image: markerImage,
-          });
-          setMap(map);
-        }
-      });
-    }
+    window.kakao.maps.load(() => {
+      if (data) {
+        setLatLng({ lat: data.lat, lng: data.lng });
+      }
+    });
   }, [data]);
+
+  useEffect(() => {
+    const drawTree = async () => {
+      if (map && data && latLng) {
+        const imageSize = new window.kakao.maps.Size(24, 24);
+        const markerImage = new window.kakao.maps.MarkerImage(treeMarker, imageSize);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const marker = new window.kakao.maps.Marker({
+          map,
+          title: data.name,
+          position: new window.kakao.maps.LatLng(latLng.lat, latLng.lng),
+          image: markerImage,
+        });
+      }
+    };
+
+    window.kakao.maps.load(() => {
+      drawTree();
+    });
+  }, [map, data, latLng]);
 
   return data ? (
     <>
