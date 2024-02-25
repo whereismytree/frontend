2cd610d161d2ef002d5e915abe2b23ab4c31dc5a
zxxng <jeayeong123@naver.com>
2024-02-25 15:54:02 +0900

    refactor: 검색시 디바운싱 적용

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import SearchInput from 'pages/MainPage/components/SearchInput';
 import { ITreeItem } from 'types/apiResponse';
-// import useApiQuery from 'hooks/useApiQuery';
+import useApiQuery from 'hooks/useApiQuery';
 import TreeImageItem from 'components/TreeImageItem';
-import treeJSON from 'assets/treedata.json';
 import * as S from './style';
 
 export const SearchPage = () => {
   const [keyword, setKeyword] = useState<string>('');
-  // TODO: 추후 서버 통신으로 변경 필요, 현재는 서버에 데이터가 없어 JSON 활용했습니다
-  const data: ITreeItem[] = treeJSON.trees;
-  // const { data } = useApiQuery<ITreeItem[]>(`v1/trees/list?name=${keyword}&address=${keyword}`);
+  // TODO: 조회안됨 백엔드 확인 필요!!
+  const { data } = useApiQuery<ITreeItem[]>(`v1/trees/list?name=${keyword}&address=${keyword}`);
 
   return (
     <>
