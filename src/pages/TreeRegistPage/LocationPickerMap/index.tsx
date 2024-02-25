37e498b750a674df33aa1708f203f4311ef40fb3
BK <3031011@naver.com>
2024-02-25 23:27:12 +0900

    refactor: 비어있는 객체인지 판별하는 isEmptyObject 유틸리티 함수로 분리

--- src/pages/TreeRegistPage/LocationPickerMap/index.tsx
+++ src/pages/TreeRegistPage/LocationPickerMap/index.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from 'components/Navbar';
 import Topbar from 'components/Topbar';
+import isEmptyObject from 'utils/isEmptyObject';
 import LocationPickerMap from './components/Map';
 import TreeRegistMapProvider from './provider';
-import * as S from './style';
 import AddressInfo from './components/AddressInfo';
 import SubmitButton from './components/SubmitButton';
+import * as S from './style';
 
 const validateLatLng = (LatLng: { lat: number; lng: number }) => {
-  const isEmptyObject = (obj: object) => Object.keys(obj).length === 0;
-
   if (!LatLng || isEmptyObject(LatLng)) {
     throw new Error('올바르지 않은 접근입니다.');
   }
