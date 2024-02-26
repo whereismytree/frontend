bf1c8d65fcbd48b1189a864f52e47b9702a63d4b
BK <3031011@naver.com>
2024-02-27 01:04:27 +0900

    feat: 요일을 반환하는 getDayofWeek 함수 Date도 인자로 받을 수 있도록 함수 오버로드

--- src/utils/dateUtils/getDayofWeek.ts
+++ src/utils/dateUtils/getDayofWeek.ts
@@ -1,5 +1,17 @@
-const getDayOfWeek = (day: ReturnType<Date['getDay']>) => {
-  const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+type DayOfWeek = '일' | '월' | '화' | '수' | '목' | '금' | '토';
+
+type getDatOfWeek = {
+  (day: ReturnType<Date['getDay']>): DayOfWeek;
+  (date: Date): DayOfWeek;
+};
+
+const getDayOfWeek: getDatOfWeek = (day: Date | ReturnType<Date['getDay']>) => {
+  const dayOfWeek: DayOfWeek[] = ['일', '월', '화', '수', '목', '금', '토'];
+
+  if (day instanceof Date) {
+    return dayOfWeek[day.getDay()];
+  }
+
   return dayOfWeek[day];
 };
 
