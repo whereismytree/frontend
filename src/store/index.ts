d3e08097c8265c5b33f5698007fddce689bae4a6
BK <3031011@naver.com>
2024-03-01 16:46:00 +0900

    refactor: 유저의 로그인 및 로그아웃 전역상태 -> 세션 스토리지에서 관리하도록 수정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import locationReducer from './modules/treeRegistLocationSlice';
-import userReducer from './modules/userInfoSlice';
 
 const rootReducer = combineReducers({
   location: locationReducer,
-  user: userReducer,
 });
 
 const store = configureStore({
