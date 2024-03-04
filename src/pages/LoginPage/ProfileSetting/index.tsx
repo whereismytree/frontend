224e3605fc09c9da98c13d427b225e517e8918d1
BK <3031011@naver.com>
2024-03-04 14:32:14 +0900

    feat: 로그인 이후 초기 프로필 설정 페이지 외부에서 URL로 접근할 수 없도록 구현

--- src/pages/LoginPage/ProfileSetting/index.tsx
+++ src/pages/LoginPage/ProfileSetting/index.tsx
@@ -1,20 +1,30 @@
 import { FormProvider, useForm } from 'react-hook-form';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ProfileImageSetting from 'pages/LoginPage/ProfileSetting/components/ImageSetting';
-import { useNavigate } from 'react-router-dom';
 import getPath from 'utils/getPath';
 import Topbar from 'components/Topbar';
+import InvalidAccess from 'components/Error/InvalidAccess';
 import { useProfile } from './hooks';
 import ProfileSettingProvider from './provider';
 import { ICreateProfileAPIBody } from './types';
-import * as S from './style';
 import SubmitButton from './components/SubmitButton';
 import NicknameSetting from './components/NicknameSetting';
+import * as S from './style';
 
 function ProfileSetting() {
+  const { state } = useLocation();
   const navigate = useNavigate();
   const { create } = useProfile();
   const methods = useForm<ICreateProfileAPIBody>({ mode: 'onChange' });
   const { handleSubmit } = methods;
+
+  if (!state) {
+    return <InvalidAccess />;
+  }
+
+  if (!state.nicknameRequire) {
+    navigate(getPath('mainPage', 'root'));
+  }
 
   const createProfile = (data: Omit<ICreateProfileAPIBody, 'profileImageUrl'>) => {
     create(
