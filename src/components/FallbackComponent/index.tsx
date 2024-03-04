79295f4fbc257026733aa92b75cc7cb345d3dac4
BEOMKWAN <126222927+KwanBeom@users.noreply.github.com>
2024-03-04 20:48:17 +0900

    refactor: 리뷰 상세 페이지 유효성 검사 에러 메시지 변경 (#66)

--- src/components/FallbackComponent/index.tsx
+++ src/components/FallbackComponent/index.tsx
@@ -1,18 +1,22 @@
 import { FallbackProps } from 'react-error-boundary';
 import Guide from 'components/common/Guide';
+import Topbar from 'components/Topbar';
 import * as S from './style';
 
 const FallbackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <S.Wrapper>
-      <Guide.Error
-        title={error.statusCode || 500}
-        text=""
-        subText={error.message}
-        btnText="다시 시도"
-        onClick={resetErrorBoundary}
-      />
-    </S.Wrapper>
+    <>
+      <Topbar>{null}</Topbar>
+      <S.Wrapper>
+        <Guide.Error
+          title={error.statusCode || 500}
+          text=""
+          subText={error.message}
+          btnText="다시 시도"
+          onClick={resetErrorBoundary}
+        />
+      </S.Wrapper>
+    </>
   );
 };
 
