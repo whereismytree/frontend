4d96d303bc5eabf12cad6276720db5c673ecfbdc
BK <3031011@naver.com>
2024-03-07 10:06:40 +0900

    refactor: 저장한 트리를 불러오는 커스텀훅 에러 발생시 HTTP에러 던지도록 수정

--- src/pages/SavedTreePage/hooks.ts
+++ src/pages/SavedTreePage/hooks.ts
@@ -1,15 +1,15 @@
+import { HTTPError } from 'error/HTTPError';
 import useApiQuery from 'hooks/useApiQuery';
 import ITreeListApiResponse from 'types/TreeListApiResponse';
 
 function useSavedTrees() {
   const { data, isError, isLoading } = useApiQuery<ITreeListApiResponse>('v1/my/trees/saved');
-  // TODO: HTTPError로 바꿔주세요.
   if (isError) {
-    throw new Error('저장한 트리를 불러오는 데 오류가 발생했습니다.');
+    throw new HTTPError('저장한 트리를 불러오는 데 오류가 발생했습니다.');
   }
 
   if (!data && !isLoading) {
-    throw new Error('저장한 트리 데이터가 없습니다!');
+    throw new HTTPError('저장한 트리 데이터가 없습니다!');
   }
 
   return data?.trees || [];
