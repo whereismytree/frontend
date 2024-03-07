fdc1c8d421b93f5605e70e4921a0836023ac69b0
BK <3031011@naver.com>
2024-03-07 21:12:10 +0900

    refactor: 내가 저장한 트리 페이지 코드 구조 개선

--- src/pages/SavedTreePage/hooks.ts
+++ src/pages/SavedTreePage/hooks.ts
@@ -4,6 +4,7 @@ import ITreeListApiResponse from 'types/TreeListApiResponse';
 
 function useSavedTrees() {
   const { data, isError, isLoading } = useApiQuery<ITreeListApiResponse>('v1/my/trees/saved');
+
   if (isError) {
     throw new HTTPError('저장한 트리를 불러오는 데 오류가 발생했습니다.');
   }
