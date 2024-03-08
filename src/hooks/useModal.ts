672ae5d580b530e678aa6c6ad04794ef6ce1b580
BK <3031011@naver.com>
2024-03-08 18:25:15 +0900

    feat: 모달을 사용할 수 있는 커스텀 훅 생성

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
+
+const createBackdrop = () => {
+  const backdrop = document.createElement('div');
+
+  backdrop.classList.add('backdrop');
+  backdrop.style.width = 'var(--content-width)';
+  backdrop.style.height = '100vh';
+  backdrop.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  backdrop.style.position = 'fixed';
+  backdrop.style.top = '0';
+  backdrop.style.zIndex = '100';
+
+  return backdrop;
+};
+
+const useModal = () => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const modalOpen = () => {
+    setIsOpen(true);
+  };
+
+  const modalClose = () => {
+    setIsOpen(false);
+    removeBackdrop();
+  };
+
+  const removeBackdrop = () => {
+    const divElement = document.querySelector('.backdrop');
+    if (divElement) divElement.remove();
+  };
+
+  useEffect(() => {
+    if (modalRef.current) {
+      if (isOpen) {
+        const backdrop = createBackdrop();
+
+        if (modalRef.current.parentElement) {
+          modalRef.current.parentElement.append(backdrop);
+        }
+
+        backdrop.addEventListener('click', () => {
+          modalClose();
+        });
+
+        document.addEventListener('keydown', (e) => {
+          if (e.key === 'Escape') {
+            modalClose();
+          }
+        });
+      }
+
+      if (!isOpen) {
+        removeBackdrop();
+      }
+
+      modalRef.current.style.display = isOpen ? 'block' : 'none';
+    }
+  }, [isOpen]);
+
+  return { open: modalOpen, close: modalClose, ref: modalRef };
+};
+
+export default useModal;
