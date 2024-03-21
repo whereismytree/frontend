3b7d79c2ce485b665908a4c67b86de751dfb2dc4
BK <3031011@naver.com>
2024-03-21 20:38:59 +0900

    fix: 마이 페이지 에러 메시지 변경
    리뷰를 불러오는 데 오류가 발생했습니다 => 내 정보를 불러오는 데 오류가 발생했습니다

--- src/pages/MyPage/hooks.ts
+++ src/pages/MyPage/hooks.ts
@@ -6,7 +6,7 @@ export const useProfile = () => {
   const { data, isError, isLoading } = useApiQuery<IUserData>('v1/my');
 
   if (isError) {
-    throw new HTTPError('리뷰를 불러오는 데 오류가 발생했습니다.');
+    throw new HTTPError('내 정보를 불러오는 데 오류가 발생했습니다.');
   }
 
   if (!data && !isLoading) {
