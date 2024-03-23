daf8cfa000fac4e58587d2f989526ceeb91cc4e6
BK <3031011@naver.com>
2024-03-23 18:24:50 +0900

    feat: 사용자의 토큰이 만료된 이후 POST, DELETE, PUT 요청 발생시에도 세션 만료 가이드 페이지로 이동하도록 수정

--- src/hooks/useApiMutation.ts
+++ src/hooks/useApiMutation.ts
@@ -1,6 +1,8 @@
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import axios, { AxiosResponse } from 'axios';
 import { HTTPError } from 'error/HTTPError';
+import getPath from 'utils/getPath';
 import { useToken } from './useUser';
 
 const useApiMutation = <TData = unknown, TVariables = unknown>(
@@ -9,11 +11,12 @@ const useApiMutation = <TData = unknown, TVariables = unknown>(
   options?: UseMutationOptions<TData, Error, TVariables, unknown>,
 ) => {
   const token = useToken();
+  const navigate = useNavigate();
 
   const mutationFn = async (variables: TVariables): Promise<TData> => {
     try {
       const response: AxiosResponse<TData> = await axios({
-        url,
+        url: `${process.env.REACT_APP_TREE_API_URL}${url}`,
         method,
         headers: {
           'Content-Type': 'application/json',
@@ -24,6 +27,10 @@ const useApiMutation = <TData = unknown, TVariables = unknown>(
 
       return response.data;
     } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 500) {
+        navigate(getPath('sessionExpired'));
+      }
+
       if (axios.isAxiosError(error) && error.response) {
         throw new HTTPError('axios 통신 중 오류가 발생했습니다.', error.response.status);
       }
